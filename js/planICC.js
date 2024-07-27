2504d33414fbc70c48fe7f03f8edc07c9503f043
Erick112358 <erickandreirias@gmail.com>
2024-07-27 11:10:29 -0600

    Más funcionamiento

--- js/planICC.js
+++ js/planICC.js
@@ -2,15 +2,22 @@ let requerimientos=()=>{
     let body;
     let clases;
     let abre;
-    let requiere;
+    let datos;
+    let clasesAbiertas;
 
     function init(){
         body=document.querySelector("body");
         clases=document.querySelectorAll(".clase");
+        datos=document.createElement("div");
+        datos.classList.add("requerimientos","hidden");        
+        body.appendChild(datos);
+
         requiere=document.createElement("div");
-        requiere.classList.add("requerimientos","hidden");
-        
-        body.appendChild(requiere);
+        abre=document.createElement("div");
+        requiere.classList.add("requiere");
+        abre.classList.add("abre");
+        datos.appendChild(requiere);
+        datos.appendChild(abre);
         clases.forEach(clase=>{
             clase.addEventListener(
                 "click",
@@ -23,15 +30,36 @@ let requerimientos=()=>{
     }
 
     function mostrarRequerimientos(clase){
-        if(requiere.innerHTML===`${clase.dataset.abre}`){
-            requiere.classList.toggle("hidden");
+        clasesAbiertas=clase.dataset.abre.split(",");
+        clasesRequeridas=clase.dataset.requiere.split(",");
+        console.log(clasesRequeridas);
+        if(abre.innerHTML===`Abre:${listarClases(clasesAbiertas)}`){
+            datos.classList.toggle("hidden");
         }
         else{
-            requiere.classList.remove("hidden");
-            requiere.innerHTML=`${clase.dataset.abre}`;
+            datos.style.transform="translateY(0px)";
+            setTimeout(() => {
+                datos.classList.remove("hidden");
+                abre.innerHTML=`Abre:${listarClases(clasesAbiertas)}`;
+                requiere.innerHTML=`Requisitos:${listarClases(clasesRequeridas)}`
+                datos.style.transform="";
+            }, 200);
+            
+        }
+    }
+
+    function listarClases(clases){
+        let string="";
+        if(clases[0]!=""){
+            clases.forEach((clase)=>{
+                string=`${string}<br>${clase}`
+            });
+        }
+        else{
+            string="<br>Ninguna";
         }
         
-        
+        return string;
     }
     
     init();
